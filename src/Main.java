3883f3625c98e2ed797206cf4f001837e4632fc5
faxcon08 <faxcon-08@mail.ru>
2022-11-23 15:03:03 +0300

    First Task 1.1;

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(ANSI_RED+"First Task 1.1"+ANSI_RESET);
+        int supplement = 15_000;
+        int total = 0;
+        int purpose = 2_459_000;
+        int month = 0;
+        while (total<=purpose){
+            total+=supplement;
+            month++;
+            System.out.println("Месяц "+month+", сумма напоплений равна "+total + " рублей");
+        }
 
-    }
+        System.out.println(ANSI_RED+"Second Task 1.2"+ANSI_RESET);
+
+    } // main
 }
